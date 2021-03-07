1418ec8c5160e0e0cb7c9d931d788b25e6750c02
timotheemcdonald <timotheemcdonald@gmail.com>
2021-03-07 11:02:08 -0500

    buttons connected to status variables

--- script.js
+++ script.js
@@ -14,10 +14,38 @@ function createPet(){
     })
 };
 
-let buttonPlay = document.getElementById('#play-action');
-let buttonFeed = document.getElementById('#food-action');
-let buttonClean = document.getElementById('#clean');
 
-buttonPlay.addEventListener('click', () => {
+let hunger = 0;
+let hygiene = 0;
+let happy = 0;
 
-})
+let happyScore = document.getElementById("happy-score");
+happyScore.innerHTML = happy;
+let hungerScore = document.getElementById("hunger-score");
+hungerScore.innerHTML = hunger;
+let hygieneScore = document.getElementById("hygiene-score");
+hygieneScore.innerHTML = hygiene;
+
+
+function play(){
+    ++happy;
+    --hygiene;
+    --hunger;
+    document.getElementById('happy-score').innerHTML = happy;
+    document.getElementById('hygiene-score').innerHTML = hygiene;
+    document.getElementById('hunger-score').innerHTML = hunger;
+}
+
+function clean(){
+    ++hygiene;
+    --happy;
+    document.getElementById('hygiene-score').innerHTML = hygiene;
+    document.getElementById('happy-score').innerHTML = happy;
+}
+
+function feed(){
+    ++hunger;
+    hygiene = hygiene - 0.5;
+    document.getElementById('hunger-score').innerHTML = hunger;
+    document.getElementById('hygiene-score').innerHTML = hygiene;
+}
